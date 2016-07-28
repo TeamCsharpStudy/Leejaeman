6a588672a3d2f02845dc8bd6b6d56f52541303ce
leejaeman <lividious@naver.com>
2016-07-28 10:49:18 +0900

    Login Program

---
+++ LoginApp/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace LoginApp
+{
+    partial class Login
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ID_Label = new System.Windows.Forms.Label();
+            this.Password_Label = new System.Windows.Forms.Label();
+            this.ID_Text = new System.Windows.Forms.TextBox();
+            this.Password_Text = new System.Windows.Forms.TextBox();
+            this.Login_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ID_Label
+            // 
+            this.ID_Label.AutoSize = true;
+            this.ID_Label.Location = new System.Drawing.Point(64, 24);
+            this.ID_Label.Name = "ID_Label";
+            this.ID_Label.Size = new System.Drawing.Size(31, 13);
+            this.ID_Label.TabIndex = 0;
+            this.ID_Label.Text = "ID :";
+            // 
+            // Password_Label
+            // 
+            this.Password_Label.AutoSize = true;
+            this.Password_Label.Location = new System.Drawing.Point(6, 51);
+            this.Password_Label.Name = "Password_Label";
+            this.Password_Label.Size = new System.Drawing.Size(94, 13);
+            this.Password_Label.TabIndex = 1;
+            this.Password_Label.Text = "Password : ";
+            // 
+            // ID_Text
+            // 
+            this.ID_Text.Location = new System.Drawing.Point(105, 21);
+            this.ID_Text.Name = "ID_Text";
+            this.ID_Text.Size = new System.Drawing.Size(114, 22);
+            this.ID_Text.TabIndex = 2;
+            // 
+            // Password_Text
+            // 
+            this.Password_Text.Location = new System.Drawing.Point(105, 49);
+            this.Password_Text.Name = "Password_Text";
+            this.Password_Text.Size = new System.Drawing.Size(114, 22);
+            this.Password_Text.TabIndex = 3;
+            this.Password_Text.UseSystemPasswordChar = true;
+            this.Password_Text.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Password_Text_KeyUp);
+            // 
+            // Login_Button
+            // 
+            this.Login_Button.Location = new System.Drawing.Point(228, 21);
+            this.Login_Button.Name = "Login_Button";
+            this.Login_Button.Size = new System.Drawing.Size(94, 49);
+            this.Login_Button.TabIndex = 4;
+            this.Login_Button.Text = "Log In";
+            this.Login_Button.UseVisualStyleBackColor = true;
+            this.Login_Button.Click += new System.EventHandler(this.Login_Button_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 89);
+            this.Controls.Add(this.Login_Button);
+            this.Controls.Add(this.Password_Text);
+            this.Controls.Add(this.ID_Text);
+            this.Controls.Add(this.Password_Label);
+            this.Controls.Add(this.ID_Label);
+            this.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Login";
+            this.Text = "Log In";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ID_Label;
+        private System.Windows.Forms.Label Password_Label;
+        private System.Windows.Forms.TextBox ID_Text;
+        private System.Windows.Forms.TextBox Password_Text;
+        private System.Windows.Forms.Button Login_Button;
+
+        private string m_id;
+        private string m_password;
+    }
+}
+
---
+++ LoginApp/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginApp
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+            m_id = "lividious";
+            m_password = "happy1234";
+        }
+
+        private void Login_Button_Click(object sender, EventArgs e)
+        {
+            CompareTotal(ID_Text.Text, Password_Text.Text);
+        }
+
+        private void Password_Text_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                CompareTotal(ID_Text.Text, Password_Text.Text);
+            }
+        }
+
+        private void CompareTotal(string id, string password)
+        {
+            bool isSameId = CompareId(id);
+            bool isSamePassword = ComparePassword(password);
+
+            if(isSameId && isSamePassword)
+                MessageBox.Show("로그인 되었습니다~~!");
+        }
+
+        private bool CompareId(string id)
+        {
+            if (m_id != id)
+            {
+                MessageBox.Show("아이디가 틀렸습니다");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ComparePassword(string password)
+        {
+            if (m_password != Password_Text.Text)
+            {
+                MessageBox.Show("비밀번호가 틀렸습니다");
+                return false;
+            }
+
+            return true;
+        }
+
+    }// end of public partial class Login : Form
+}
